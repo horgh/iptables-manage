51571b2ae48f853f208b2dc8b1b1b8cee4c1bf46
Will Storey <will@summercat.com>
2017-04-01 13:58:12 -0700

    cidrlist: Rewrite file every time we call RecordIP
    We call RecordIP when there is an IP that should be permitted.
    Previously we would not update the file if the IP was already listed. We
    do now. We do this by writing out to a temporary file and then moving it
    into place.
    
    We rewrite always for two reasons:
      1. Track last time an IP was seen (so old can be pruned)
      2. This causes iptables-manage, when running as a daemon, to sync the
         rules (since the file changed).

--- cidrlist/cidrlist.go
+++ cidrlist/cidrlist.go
@@ -10,37 +10,173 @@ package cidrlist
 import (
 	"bufio"
 	"fmt"
+	"io/ioutil"
 	"log"
 	"net"
 	"os"
+	"path/filepath"
 	"strings"
 	"syscall"
+	"time"
 )
+
+// Record holds information about a single CIDR list entry.
+type Record struct {
+	Net     *net.IPNet
+	Comment string
+}
 
 // RecordIP records the IP to the CIDR file.
 //
-// The format for the IP is a line by itself containing:
-// ip/32
+// The format we write, every time:
+// # Comment @ current time\n
+// cidr\n
 //
-// We do not write anything to the file if the IP is already present.
-func RecordIP(file, ipStr, comment string) error {
+// For example:
+// # A great IP @ Thu, 21 Jul 2016 22:45:17 PDT
+// 192.168.1.32/32
+//
+// If the IP is already in the file, then we rewrite the file without the IP,
+// then add it back (leaving the CIDR as it was in the file) with the given
+// comment at the end of the file. This lets us see most recently used IPs at
+// the bottom of the file.
+//
+// Writing a record even if the IP is present also means that if
+// iptables-manage is monitoring the file, then it will re-sync the rules even
+// if the IP is already allowed. This is useful in case the rules somehow got
+// into an inconsistent state (such as through reloading rules from a
+// snapshot).
+func RecordIP(file, ipStr, comment string, t time.Time) error {
 	ip := net.ParseIP(ipStr)
 	if ip == nil {
 		return fmt.Errorf("invalid IP: %s", ipStr)
 	}
 
-	alreadyRecorded, err := ipIsInFile(file, ip)
+	lfh, err := lockFile(file)
 	if err != nil {
-		return fmt.Errorf("unable to check if IP is in file: %s", err)
+		return fmt.Errorf("unable to lock file: %s: %s", file, err)
 	}
 
-	if alreadyRecorded {
-		return nil
+	defer func() {
+		err = lfh.Close()
+		if err != nil {
+			log.Printf("Error closing lock on file: %s: %s", file, err)
+		}
+	}()
+
+	// Read in current records.
+	records, err := LoadCIDRsFromFile(file)
+	if err != nil {
+		if !os.IsNotExist(err) {
+			return fmt.Errorf("unable to load records: %s", err)
+		}
 	}
 
-	fh, err := os.OpenFile(file, os.O_WRONLY|os.O_CREATE|os.O_APPEND, 0644)
+	// Write records out to temporary file. Skip the IP if it is already present.
+
+	// Find directory the file is in so we create the temporary file in the same
+	// one. This is because we will rename it after we finish writing to replace
+	// the old, and we want it on the same filesystem. Otherwise moving is not
+	// atomic. As we have a lock it is not a huge concern if it is not atomic, but
+	// we may as well do this.
+	dir := filepath.Dir(file)
+
+	fh, err := ioutil.TempFile(dir, "iptables-manage")
 	if err != nil {
-		return fmt.Errorf("unable to open: %s: %s", file, err)
+		return fmt.Errorf("unable to create temporary file: %s", err)
+	}
+
+	tempName := fh.Name()
+
+	// Track network that already contains the IP in the file (if any).
+	var ipNet *net.IPNet
+
+	for _, rec := range records {
+		if rec.Net.Contains(ip) {
+			// Raise error if IP is listed in two CIDRs. We need to resolve this. We
+			// probably want the broader CIDR, but let manually fix it for now.
+			if ipNet != nil {
+				_ = fh.Close()
+				_ = os.Remove(tempName)
+				return fmt.Errorf("ip is listed twice: %s: %s and %s", ip, ipNet,
+					rec.Net)
+			}
+
+			ipNet = rec.Net
+			continue
+		}
+
+		output := fmt.Sprintf("# %s\n%s\n", rec.Comment, rec.Net)
+
+		if err := writeFull(fh, output); err != nil {
+			_ = fh.Close()
+			_ = os.Remove(tempName)
+			return fmt.Errorf("unable to write: %s: %s", tempName, err)
+		}
+	}
+
+	// Write our IP. Retain the CIDR we found if it was in the file already. It may
+	// be broader than /32.
+
+	output := ""
+
+	if ipNet != nil {
+		output = fmt.Sprintf("# %s @ %s\n%s\n", comment, t.Format(time.RFC1123),
+			ipNet)
+	} else {
+		output = fmt.Sprintf("# %s @ %s\n%s/32\n", comment, t.Format(time.RFC1123),
+			ip)
+	}
+
+	if err := writeFull(fh, output); err != nil {
+		_ = fh.Close()
+		_ = os.Remove(tempName)
+		return fmt.Errorf("unable to write: %s: %s", tempName, err)
+	}
+
+	if err := fh.Close(); err != nil {
+		_ = os.Remove(tempName)
+		return fmt.Errorf("close failed: %s: %s", file, err)
+	}
+
+	// Move it into place.
+
+	if err := os.Rename(tempName, file); err != nil {
+		_ = os.Remove(tempName)
+		return fmt.Errorf("unable to replace file: %s to %s: %s", tempName, file,
+			err)
+	}
+
+	return nil
+}
+
+// Acquire a lock on the file. We don't lock the file itself, but instead the
+// file with a .lock suffix.
+//
+// Why not lock the file itself? Consider the case where we want to rewrite the
+// file. If we did that, other processes could have the old file open after we
+// release the lock:
+//
+// - P0 opens the file F0
+// - P0 acquires lock on F0
+// - P1 opens the file F0
+// - P1 tries to acquire lock on F0 and waits
+// - P0 replaces file F0 with F1 (moving a F1 over to replace F0)
+// - P0 relinquishes lock on F0
+// - P1 acquires lock on F0. Examines file F0 and does work on it. But it should
+//   be looking at F1 now.
+//
+// If we instead use a lock file then P1 would open F1 after it acquires the
+// lock on the lock file, and avoid the above issue.
+//
+// Note this method currently assumes leaving the .lock file in place, as
+// removing it will lead to a similar race on the .lock file itself.
+func lockFile(file string) (*os.File, error) {
+	lockFilename := fmt.Sprintf("%s.lock", file)
+
+	fh, err := os.OpenFile(lockFilename, os.O_WRONLY|os.O_CREATE, 0644)
+	if err != nil {
+		return nil, fmt.Errorf("unable to open: %s: %s", lockFilename, err)
 	}
 
 	flockt := syscall.Flock_t{
@@ -48,43 +184,24 @@ func RecordIP(file, ipStr, comment string) error {
 	}
 	err = syscall.FcntlFlock(fh.Fd(), syscall.F_SETLKW, &flockt)
 	if err != nil {
-		return fmt.Errorf("unable to lock file: %s: %s", file, err)
+		_ = fh.Close()
+		return nil, fmt.Errorf("unable to lock file: %s: %s", lockFilename, err)
 	}
 
-	output := fmt.Sprintf("# %s\n%s/32\n", comment, ip)
+	return fh, nil
+}
 
-	sz, err := fh.WriteString(output)
+func writeFull(fh *os.File, s string) error {
+	sz, err := fh.WriteString(s)
 	if err != nil {
-		_ = fh.Close()
 		return fmt.Errorf("unable to write: %s", err)
 	}
 
-	if sz != len(output) {
-		_ = fh.Close()
+	if sz != len(s) {
 		return fmt.Errorf("short write")
 	}
 
-	if err := fh.Close(); err != nil {
-		return fmt.Errorf("close failed: %s: %s", file, err)
-	}
-
 	return nil
-}
-
-// ipIsInFile checks if the IP is listed in the file.
-func ipIsInFile(file string, ip net.IP) (bool, error) {
-	nets, err := LoadCIDRsFromFile(file)
-	if err != nil {
-		return false, fmt.Errorf("unable to load CIDRs: %s", err)
-	}
-
-	for _, n := range nets {
-		if n.Contains(ip) {
-			return true, nil
-		}
-	}
-
-	return false, nil
 }
 
 // LoadCIDRsFromFile opens and parse the CIDR file.
@@ -92,44 +209,58 @@ func ipIsInFile(file string, ip net.IP) (bool, error) {
 // We ignore # comments and blank lines.
 //
 // All other lines must be full CIDRs. We parse them.
-func LoadCIDRsFromFile(file string) ([]*net.IPNet, error) {
+func LoadCIDRsFromFile(file string) ([]Record, error) {
 	fh, err := os.Open(file)
 	if err != nil {
-		return nil, fmt.Errorf("unable to open file: %s: %s", file, err)
+		return nil, err
 	}
 
 	defer func() {
 		err := fh.Close()
 		if err != nil {
-			log.Printf("close failure: %s: %s", file, err)
+			log.Printf("Failure closing file: %s: %s", file, err)
 		}
 	}()
 
 	scanner := bufio.NewScanner(fh)
 
-	var cidrs []*net.IPNet
+	records := []Record{}
+	comment := ""
 
 	for scanner.Scan() {
 		line := scanner.Text()
+
 		line = strings.TrimSpace(line)
 		if len(line) == 0 {
 			continue
 		}
+
 		if line[0] == '#' {
+			if len(line) > 2 {
+				// From "# test" take "test"
+				comment = line[2:]
+			} else {
+				comment = ""
+			}
 			continue
 		}
 
 		_, ipNet, err := net.ParseCIDR(line)
 		if err != nil {
-			return nil, fmt.Errorf("invalid CIDR: %s: %s", line, err)
+			return nil, err
 		}
 
-		cidrs = append(cidrs, ipNet)
+		records = append(records, Record{
+			Net:     ipNet,
+			Comment: comment,
+		})
+
+		comment = ""
 	}
 
 	if scanner.Err() != nil {
 		return nil, fmt.Errorf("file scan error: %s", scanner.Err())
 	}
 
-	return cidrs, nil
+	return records, nil
 }
---
+++ cidrlist/cidrlist_test.go
@@ -0,0 +1,401 @@
+package cidrlist
+
+import (
+	"fmt"
+	"io/ioutil"
+	"net"
+	"os"
+	"testing"
+	"time"
+)
+
+func TestRecordIP(t *testing.T) {
+	tests := []struct {
+		IP            string
+		Comment       string
+		PriorContents string
+		CreateFile    bool
+		Records       []Record
+		WantError     error
+	}{
+		// Invalid IP.
+		{
+			IP:            "junk",
+			Comment:       "",
+			PriorContents: "",
+			CreateFile:    false,
+			Records:       []Record{},
+			WantError:     fmt.Errorf("invalid IP: junk"),
+		},
+
+		// File doesn't exist.
+		{
+			IP:            "192.168.1.3",
+			Comment:       "test 1 2 3",
+			PriorContents: "",
+			CreateFile:    false,
+			Records: []Record{
+				Record{
+					Net: &net.IPNet{
+						IP:   net.ParseIP("192.168.1.3"),
+						Mask: net.CIDRMask(32, 32),
+					},
+					Comment: "test 1 2 3",
+				},
+			},
+			WantError: nil,
+		},
+
+		// Invalid record present.
+		{
+			IP:            "192.168.1.3",
+			Comment:       "test 1 2 3",
+			PriorContents: "junk\n",
+			CreateFile:    true,
+			Records:       []Record{},
+			WantError:     fmt.Errorf("unable to load records: invalid CIDR address: junk"),
+		},
+
+		// IP is listed twice.
+		{
+			IP:            "192.168.1.3",
+			Comment:       "test 1 2 3",
+			PriorContents: "192.168.1.3/32\n192.168.1.0/24\n",
+			CreateFile:    true,
+			Records:       []Record{},
+			WantError:     fmt.Errorf("ip is listed twice: 192.168.1.3: 192.168.1.3/32 and 192.168.1.0/24"),
+		},
+
+		// IP is not in file yet.
+		{
+			IP:            "192.168.1.4",
+			Comment:       "test 1 2 3",
+			PriorContents: "192.168.1.3/32\n",
+			CreateFile:    true,
+			Records: []Record{
+				Record{
+					Net: &net.IPNet{
+						IP:   net.ParseIP("192.168.1.3"),
+						Mask: net.CIDRMask(32, 32),
+					},
+					Comment: "",
+				},
+				Record{
+					Net: &net.IPNet{
+						IP:   net.ParseIP("192.168.1.4"),
+						Mask: net.CIDRMask(32, 32),
+					},
+					Comment: "test 1 2 3",
+				},
+			},
+			WantError: nil,
+		},
+
+		// IP is in as a different CIDR already.
+		{
+			IP:            "192.168.1.4",
+			Comment:       "test 1 2 3",
+			PriorContents: "# nice comment\n192.168.1.0/24\n# another nice comment\n192.168.2.0/24",
+			CreateFile:    true,
+			Records: []Record{
+				Record{
+					Net: &net.IPNet{
+						IP:   net.ParseIP("192.168.2.0"),
+						Mask: net.CIDRMask(24, 32),
+					},
+					Comment: "another nice comment",
+				},
+				Record{
+					Net: &net.IPNet{
+						IP:   net.ParseIP("192.168.1.0"),
+						Mask: net.CIDRMask(24, 32),
+					},
+					Comment: "test 1 2 3",
+				},
+			},
+			WantError: nil,
+		},
+	}
+
+	tempName, err := getTempFilename()
+	if err != nil {
+		t.Errorf("creating temp file: %s", err)
+		return
+	}
+
+	recTime := time.Now()
+
+	for _, test := range tests {
+		if err := ioutil.WriteFile(tempName, []byte(test.PriorContents), 0644); err != nil {
+			t.Errorf("unable to write to file: %s: %s: %s", tempName,
+				test.PriorContents, err)
+			continue
+		}
+
+		err := RecordIP(tempName, test.IP, test.Comment, recTime)
+		if !errorsEqual(err, test.WantError) {
+			t.Errorf("RecordIP(%s, %s, %s, %s) = error %s, wanted error %s", tempName,
+				test.IP, test.Comment, recTime, err, test.WantError)
+			_ = os.Remove(tempName)
+			continue
+		}
+
+		if err != nil {
+			_ = os.Remove(tempName)
+			continue
+		}
+
+		recs, err := LoadCIDRsFromFile(tempName)
+		if !errorsEqual(err, test.WantError) {
+			t.Errorf("LoadCIDRsFromFile() error = %s", err)
+			_ = os.Remove(tempName)
+			continue
+		}
+
+		_ = os.Remove(tempName)
+
+		// Add time to last record
+		test.Records[len(test.Records)-1].Comment = fmt.Sprintf("%s @ %s",
+			test.Records[len(test.Records)-1].Comment, recTime.Format(time.RFC1123))
+
+		if err := recordsEqual(recs, test.Records); err != nil {
+			t.Errorf("records = %v, wanted %v. mismatch is: %s", recs, test.Records,
+				err)
+			continue
+		}
+	}
+}
+
+func TestWriteFull(t *testing.T) {
+	tests := []struct {
+		Input string
+	}{
+		{"hi"},
+		{""},
+	}
+
+	for _, test := range tests {
+		fh, err := ioutil.TempFile("", "test")
+		if err != nil {
+			t.Errorf("TempFile(\"\", \"test\") = error %s", err)
+			return
+		}
+
+		if err := writeFull(fh, test.Input); err != nil {
+			t.Errorf("writeFull(fh, %s) = error %s", test.Input, err)
+			_ = fh.Close()
+			_ = os.Remove(fh.Name())
+			continue
+		}
+
+		if err := fh.Close(); err != nil {
+			t.Errorf("writeFull: close: %s", err)
+			_ = os.Remove(fh.Name())
+			return
+		}
+
+		buf, err := ioutil.ReadFile(fh.Name())
+		if err != nil {
+			t.Errorf("writeFull: reading file back: %s", err)
+			_ = os.Remove(fh.Name())
+			return
+		}
+
+		if string(buf) != test.Input {
+			t.Errorf("writeFull: read back different than wrote: %s vs %s", buf,
+				test.Input)
+			_ = os.Remove(fh.Name())
+			continue
+		}
+
+		if err := os.Remove(fh.Name()); err != nil {
+			t.Errorf("writeFull: remove: %s", err)
+			return
+		}
+	}
+}
+
+func TestLoadCIDRsFromFile(t *testing.T) {
+	tests := []struct {
+		Contents  string
+		Records   []Record
+		WantError error
+	}{
+		{
+			Contents:  "",
+			Records:   []Record{},
+			WantError: nil,
+		},
+		{
+			Contents: "# test\n192.168.1.0/24\n",
+			Records: []Record{
+				Record{
+					Net: &net.IPNet{
+						IP:   net.ParseIP("192.168.1.0"),
+						Mask: net.CIDRMask(24, 32),
+					},
+					Comment: "test",
+				},
+			},
+			WantError: nil,
+		},
+		{
+			Contents: "192.168.1.0/24\n",
+			Records: []Record{
+				Record{
+					Net: &net.IPNet{
+						IP:   net.ParseIP("192.168.1.0"),
+						Mask: net.CIDRMask(24, 32),
+					},
+				},
+			},
+			WantError: nil,
+		},
+		{
+			Contents: "\n192.168.1.0/24\n\n",
+			Records: []Record{
+				Record{
+					Net: &net.IPNet{
+						IP:   net.ParseIP("192.168.1.0"),
+						Mask: net.CIDRMask(24, 32),
+					},
+				},
+			},
+			WantError: nil,
+		},
+		{
+			Contents: "# test 1 2\n192.168.1.0/24\n# test 3 4\n192.168.0.0/16\n",
+			Records: []Record{
+				Record{
+					Net: &net.IPNet{
+						IP:   net.ParseIP("192.168.1.0"),
+						Mask: net.CIDRMask(24, 32),
+					},
+					Comment: "test 1 2",
+				},
+				Record{
+					Net: &net.IPNet{
+						IP:   net.ParseIP("192.168.0.0"),
+						Mask: net.CIDRMask(16, 32),
+					},
+					Comment: "test 3 4",
+				},
+			},
+			WantError: nil,
+		},
+		{
+			Contents:  "# test 1 2\n192.168.1.0/24\njunk\n",
+			Records:   []Record{},
+			WantError: fmt.Errorf("invalid CIDR address: junk"),
+		},
+	}
+
+	tempName, err := getTempFilename()
+	if err != nil {
+		t.Errorf("creating temp file: %s", err)
+		return
+	}
+
+	// Try when file does not exist.
+	_, err = LoadCIDRsFromFile(tempName)
+	if !os.IsNotExist(err) {
+		t.Errorf("unexpected error when file does not exist: %s", err)
+		return
+	}
+
+	for _, test := range tests {
+		if err := ioutil.WriteFile(tempName, []byte(test.Contents), 0644); err != nil {
+			t.Errorf("unable to write to file: %s: %s: %s", tempName, test.Contents,
+				err)
+			continue
+		}
+
+		recs, err := LoadCIDRsFromFile(tempName)
+		if !errorsEqual(err, test.WantError) {
+			t.Errorf("LoadCIDRsFromFile() contents = %s, error = %s, wanted error %s",
+				test.Contents, err, test.WantError)
+			_ = os.Remove(tempName)
+			continue
+		}
+
+		if err := os.Remove(tempName); err != nil {
+			t.Errorf("removing file: %s: %s", tempName, err)
+			continue
+		}
+
+		if err != nil {
+			continue
+		}
+
+		if err := recordsEqual(recs, test.Records); err != nil {
+			t.Errorf("LoadCIDRsFromFile contents = %s, records = %v, wanted %v. mismatch is: %s",
+				test.Contents, recs, test.Records, err)
+			continue
+		}
+	}
+}
+
+func errorsEqual(err0, err1 error) bool {
+	if err0 == nil && err1 == nil {
+		return true
+	}
+	if err0 == nil && err1 != nil {
+		return false
+	}
+	if err0 != nil && err1 == nil {
+		return false
+	}
+	s0 := fmt.Sprintf("%s", err0)
+	s1 := fmt.Sprintf("%s", err1)
+	return s0 == s1
+}
+
+func recordsEqual(recs0, recs1 []Record) error {
+	if recs0 == nil && recs1 == nil {
+		return nil
+	}
+	if recs0 == nil && recs1 != nil {
+		return fmt.Errorf("one nil, one not")
+	}
+	if recs0 != nil && recs1 == nil {
+		return fmt.Errorf("one nil, one not")
+	}
+
+	if len(recs0) != len(recs1) {
+		return fmt.Errorf("record count mismatch: %d vs %d", len(recs0), len(recs1))
+	}
+
+	for i := range recs0 {
+		if recs0[i].Net.String() != recs1[i].Net.String() {
+			return fmt.Errorf("record %d net mismatch: %s vs %s", i,
+				recs0[i].Net.String(), recs1[i].Net.String())
+		}
+
+		if recs0[i].Comment != recs1[i].Comment {
+			return fmt.Errorf("record %d comment mismatch: %s vs %s", i,
+				recs0[i].Comment, recs1[i].Comment)
+		}
+	}
+
+	return nil
+}
+
+func getTempFilename() (string, error) {
+	fh, err := ioutil.TempFile("", "test")
+	if err != nil {
+		return "", fmt.Errorf("unable to create temp file: %s", err)
+	}
+
+	tempName := fh.Name()
+
+	if err := fh.Close(); err != nil {
+		_ = os.Remove(tempName)
+		return "", fmt.Errorf("closing file: %s", err)
+	}
+
+	if err := os.Remove(tempName); err != nil {
+		return "", fmt.Errorf("removing file: %s", err)
+	}
+
+	return tempName, nil
+}
